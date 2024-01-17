ab9a6d4656c60ef19912b47fd2e884ec2ebc6eba
em008 <100390334+em008@users.noreply.github.com>
2024-01-16 19:28:58 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,11 @@
+"""
+This code creates a chatbot that can answer questions based on a given PDF file.
+The first few lines of the code import the necessary libraries and modules.
+The `CreateChatbot()` function creates a chatbot that can answer questions based on a given PDF file. The function prompts the user to enter the file path of the PDF file, loads the PDF file, and creates a `RetrievalQA` object that can be used to answer questions based on the PDF file.
+The `while` loop at the end of the code runs the chatbot. It prompts the user to enter a question, and then uses the `RetrievalQA` object to generate a response to the question.
+This code requires the user to have a Hugging Face API token, which is used to access the Hugging Face model. The user is prompted to enter the API token when the code is run.
+"""
+
 from langchain_community.llms import HuggingFaceHub
 from src.pdfloader import load_pdf
 from src.prompt import pdfreaderprompt
