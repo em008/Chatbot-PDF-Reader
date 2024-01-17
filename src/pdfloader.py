deae7908b6db5f453aba90432b70e3ab68517730
em008 <100390334+em008@users.noreply.github.com>
2024-01-16 19:35:18 -0500

    Update pdfloader.py

--- src/pdfloader.py
+++ src/pdfloader.py
@@ -1,3 +1,13 @@
+"""
+This code defines a function `load_pdf` that loads an unstructured PDF file and creates a vector store that can be used to retrieve information from the PDF file.
+The `load_pdf` function is defined to load an unstructured PDF file and create a vector store that can be used to retrieve information from the PDF file.
+The `UnstructuredPDFLoader` class from the `langchain_community.document_loaders` module is used to load the PDF file.
+The `CharacterTextSplitter` class from the `langchain.text_splitter` module is used to split the PDF file into smaller chunks of text.
+The `HuggingFaceEmbeddings` class from the `langchain_community.embeddings` module is used to generate embeddings for the text chunks.
+The `Chroma` class from the `langchain_community.vectorstores` module is used to create a vector store that can be used to retrieve information from the PDF file.
+The `load_pdf` function takes a file path as input and returns a `Chroma` object that can be used to retrieve information from the PDF file.
+"""
+
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.document_loaders import UnstructuredPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
