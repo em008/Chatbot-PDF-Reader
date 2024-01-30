05038d164a3072f43503b28bf64fdf413dca4db3
em008 <100390334+em008@users.noreply.github.com>
2024-01-30 17:30:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+from flask import Flask, render_template, request # Import libraries
+import chatbot # Import ML model function module
 
+# Initialize Flask app
+app = Flask(__name__)
+
+# Default home page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/get')
+def get_bot_response():
+    user_text = request.args.get('msg')
+    return str(chatbot.CreateChatbot().invoke(user_text))
+
+if __name__ == "__main__":
+    app.run()
+  
